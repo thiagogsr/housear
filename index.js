657a61d827580d5be88ef92559728d60896a7b5a
Thiago Guimarães <thiagogsr@gmail.com>
2018-04-29 23:50:41 -0300

    Remove server

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const http = require("http")
 const livolo = require("./modules/livolo")
 
-const hostname = "127.0.0.1"
-const port = 3000
-
-const server = http.createServer(function(request, response) {
-  response.statusCode = 200
-  response.setHeader("Content-Type", "text/plain")
-  response.end("Housear\n")
-})
-
-server.listen(port, hostname, () => {
-  livolo.start()
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+livolo.start()
