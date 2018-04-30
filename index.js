4f7ec433000ad040f6230aa85bc900688cb95c37
Thiago Guimarães <thiagogsr@gmail.com>
2018-04-29 23:41:40 -0300

    Create server

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-var livolo = require("./modules/livolo")
+const http = require("http")
+const livolo = require("./modules/livolo")
 
-livolo.start()
+const hostname = "127.0.0.1"
+const port = 3000
+
+const server = http.createServer(function(request, response) {
+  response.statusCode = 200
+  response.setHeader("Content-Type", "text/plain")
+  response.end("Housear\n")
+})
+
+server.listen(port, hostname, () => {
+  livolo.start()
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
